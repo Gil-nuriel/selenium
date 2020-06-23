1503fe59fc23b6635147d70db39f1d9460fc7b29
Gil-nuriel <gilnrl11@gmail.com>
2020-06-23 11:12:29 +0300

    change directories

--- Tests/allTests.py
+++ Tests/allTests.py
@@ -1,17 +1,12 @@
 import unittest
 import time
 import HtmlTestRunner
-import os, sys, inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir+'\Resources')
-sys.path.insert(0, parentdir+'\Drivers')
-sys.path.insert(0, parentdir+'\Resources\PO')
-from Locators import Locators
-from TestData import TestData
-from PO import tests_layers
+from ..Resources.Locators import Locators
+from ..Resources.TestData import TestData
+from ..Resources.PO import tests_layers
 from tests_layers import Google, Amazon
-from driver import ChromeDriver, FirefoxDriver
+from ..Drivers.driver import ChromeDriver, FirefoxDriver
+
 
 class Test_001_Google(ChromeDriver):
 
@@ -23,11 +18,14 @@ class Test_001_Google(ChromeDriver):
         self.google = Google(self.driver)
         self.google.search_google2()
 
+
 class Test_002_Amazon(ChromeDriver):
 
     def test_001_user_try_to_search_on_amazon(self):
         self.amazon = Amazon(self.driver)
         self.amazon.search_amazon()
 
+
 if __name__ == '__main__':
-    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=parentdir + '\Reports'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(
+        output=parentdir + '\Reports'))
