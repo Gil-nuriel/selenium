53a77da2785d2d63bee0ca865c628ece7be5e99c
Giln <giln@empow.co>
2020-06-25 17:54:58 +0300

    remove some options

--- Drivers/driver.py
+++ Drivers/driver.py
@@ -5,8 +5,6 @@ import sys, os
 class Driver(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         if(os.environ.get("CHROME")):
             self.driver = webdriver.Chrome(options=chrome_options)
