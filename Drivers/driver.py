38e3aa5c6b2616ee278f59ff1b825298cb5cd96f
Giln <giln@empow.co>
2020-06-25 17:03:52 +0300

    try adding chrome options to driver

--- Drivers/driver.py
+++ Drivers/driver.py
@@ -4,8 +4,12 @@ import sys, os
 
 class Driver(unittest.TestCase):
     def setUp(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         if(os.environ.get("CHROME")):
-            self.driver = webdriver.Chrome()
+            self.driver = webdriver.Chrome(chrome_options=chrome_options)
         elif(os.environ.get("FIREFOX")):
             self.driver = webdriver.Firefox()
 
